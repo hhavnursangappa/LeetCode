04a9f56a4f87cd0d13a1e20871560d1df3eff522
hhavnursangappa <78108095+hhavnursangappa@users.noreply.github.com>
2024-01-27 15:20:37 +0100

    Time: 0 ms (100.00%), Space: 12 MB (11.67%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
-    int searchInsert(vector<int>& nums, int trgt) {
-        int b = 0; int e = nums.size(); int m = (e+b) / 2; 
-        while (e > b){
-            if (nums[m] == trgt)
-                return m; 
-            else if (nums[m] > trgt)
-                e = m; 
-            else 
-                b = m + 1; 
-            m = (b+e)/ 2;
+    int searchInsert(vector<int>& nums, int target) {
+        int b = 0;
+        int e = nums.size();
+        int m = (e+b)/2;
+        while(b<e)
+        {            
+            if (nums[m] == target)
+                return m;
+            else if (nums[m] > target)
+                e = m;
+            else
+                b = m+1;
+            m = (e + b) / 2;
         }
-        // if( m < nums.size() && m > )
-        return m; 
+        return m;
     }
+    
+    // int searchInsert(vector<int>& nums, int trgt) {
+    //     DPSolver;
+    //     int b = 0; int e = nums.size(); int m = (e+b) / 2; 
+    //     while (e > b){
+    //         if (nums[m] == trgt)
+    //             return m; 
+    //         else if (nums[m] > trgt)
+    //             e = m; 
+    //         else 
+    //             b = m + 1; 
+    //         m = (b+e)/ 2;
+    //     }
+    //     // if( m < nums.size() && m > )
+    //     return m; 
+    // }
 };
