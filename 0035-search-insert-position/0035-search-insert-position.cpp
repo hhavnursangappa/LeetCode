4d010fc9332b9d45ef30b2b8f233b511aa75d8b9
hhavnursangappa <78108095+hhavnursangappa@users.noreply.github.com>
2024-01-27 15:20:49 +0100

    Time: 3 ms (76.73%), Space: 12 MB (11.67%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -17,19 +17,4 @@ public:
         return m;
     }
     
-    // int searchInsert(vector<int>& nums, int trgt) {
-    //     DPSolver;
-    //     int b = 0; int e = nums.size(); int m = (e+b) / 2; 
-    //     while (e > b){
-    //         if (nums[m] == trgt)
-    //             return m; 
-    //         else if (nums[m] > trgt)
-    //             e = m; 
-    //         else 
-    //             b = m + 1; 
-    //         m = (b+e)/ 2;
-    //     }
-    //     // if( m < nums.size() && m > )
-    //     return m; 
-    // }
 };
