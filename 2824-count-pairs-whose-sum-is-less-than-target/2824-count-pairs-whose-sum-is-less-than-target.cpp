a852a32f2a2c47bd16ff47176505a3706c6736df
hhavnursangappa <78108095+hhavnursangappa@users.noreply.github.com>
2024-01-29 20:33:11 +0100

    Time: 7 ms (53.97%), Space: 23.2 MB (11.08%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
-        std::vector<std::vector<int>> final_res;
+        // std::vector<std::vector<int>> final_res;
+        
         int pair_count = 0;
         for (int ii=0; ii<nums.size(); ii++)
         {
             std::vector<int> num_pairs;
-            for (int jj=1; jj<=nums.size()-1; jj++)
+            for (int jj=ii+1; jj<=nums.size()-1; jj++)
             {
-                if ((ii < jj) && (nums[ii] + nums[jj] < target))
+                if (nums[ii] + nums[jj] < target)
                     pair_count++;
             }
         }
+        
+        // std::sort(nums.begin(), nums.end());
+        // for (int ii=0; ii<nums.size()-1; ii++)
+        // {
+        //     if (nums[ii] + nums[ii+1] < target)
+        //         pair_count++;
+        // }
         return pair_count;
     }
 };
