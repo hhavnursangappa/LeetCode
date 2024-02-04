c107b0278549bfa84a53f55c3390318880f6fa6c
hhavnursangappa <78108095+hhavnursangappa@users.noreply.github.com>
2024-02-04 13:02:11 +0100

    Time: 14 ms (29.84%), Space: 16.2 MB (18.67%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,43 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        std::unordered_map<int, int> hash_table;
+        std::unordered_map<int, std::vector<int>> hash_table;
         std::vector<vector<int>> result;
-
-        // std::sort(groupSizes.begin(), groupSizes.end())
-        for(int ii=0; ii<groupSizes.size(); ii++)
-        {
-            hash_table[groupSizes[ii]]++;             
-        }
         
-        for(auto x: hash_table)
+        for (int ii=0; ii<groupSizes.size(); ii++)
         {
-            int continue_idx =0;
-            for (int ii=0; ii<(x.second / x.first); ii++)
+            hash_table[groupSizes[ii]].push_back(ii);
+            if (hash_table[groupSizes[ii]].size() == groupSizes[ii])
             {
-                std::vector<int> vec;                
-                for(int jj=continue_idx; jj<groupSizes.size(); jj++)
-                {
-                    if (groupSizes[jj] == x.first)
-                    {
-                        if (vec.size() < x.first)
-                        {
-                            vec.push_back(jj);
-                        }                            
-                        else
-                        {
-                            continue_idx=jj;
-                            // result.push_back(vec);
-                            break;
-                        }
-                    }
-                        
-                }
-                result.push_back(vec);
+                result.push_back(hash_table[groupSizes[ii]]);
+                hash_table[groupSizes[ii]].clear();
             }
-                        
         }
         return result;
+
+        
         
     }
 };
