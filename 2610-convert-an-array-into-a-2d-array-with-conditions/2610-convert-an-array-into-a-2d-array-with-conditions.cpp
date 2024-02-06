f6de6e2624299c1d4449c6f1511dc8dbf046819d
hhavnursangappa <78108095+hhavnursangappa@users.noreply.github.com>
2024-02-06 20:22:17 +0100

    Time: 11 ms (89.30%), Space: 34.2 MB (5.05%) - LeetHub

---
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> findMatrix(vector<int>& nums) 
+    {
+        
+        std::unordered_map<int, int> hash_map;
+        int max_freq=0;
+        for (int ii=0; ii<nums.size(); ii++)
+        {
+            hash_map[nums[ii]]++;
+            max_freq = max(max_freq,  hash_map[nums[ii]]);
+        }
+        
+        vector<vector<int>> ans(max_freq);       
+        
+        int idx_track=0;
+        for (int kk=0; kk<nums.size(); kk++)
+        {
+            for (int jj=0; jj<max_freq; jj++)
+            {
+                if (std::find(ans[jj].begin(), ans[jj].end(), nums[kk]) == ans[jj].end())
+                {
+                    ans[jj].push_back(nums[kk]);
+                    break;
+                }
+            }
+        }           
+        return ans;
+    }
+};
